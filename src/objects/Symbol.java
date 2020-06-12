266936672a79c5c58993f1482dcac86da8f93e60
cheapstockquotes <ggarrisonsr@hotmail.com>
2020-06-12 04:11:17 -0400

    Addition to Asset List - name, sector, type, country

--- src/objects/Symbol.java
+++ src/objects/Symbol.java
@@ -3,9 +3,21 @@ package objects;
 
 public class Symbol {
 	private String symbol;
+	private String type;
+	private String country;
+	private String sector;
+	private String name;
 
 	public Symbol(String symbol) {
 		this.symbol = symbol;
+	}
+
+	public Symbol(String symbol, String name, String type, String sector, String country) {
+		this.symbol = symbol;
+		this.name = name;
+		this.type = type;
+		this.sector = sector;
+		this.country = country;
 	}
 
 	public String getSymbol() {
@@ -14,6 +26,23 @@ public class Symbol {
 
 	@Override
 	public String toString() {
-		return symbol;
+		return symbol + " " + name + " " + type + " " + sector + " " + country;
 	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getSector() {
+		return sector;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 }
